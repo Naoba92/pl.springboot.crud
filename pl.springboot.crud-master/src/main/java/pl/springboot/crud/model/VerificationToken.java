53b15ffcf4749d3c63ff7a9cfbb9236b66e8af78
Folfek22 <folfoszynski.mateusz@gmail.com>
2021-09-05 10:04:10 +0200

    Revert "Revert "Added new users email verification, change Springboot Version""
    This reverts commit 3bc790e277a2e01e85e35c4cdc15f58e7c31261c.

--- pl.springboot.crud-master/src/main/java/pl/springboot/crud/model/VerificationToken.java
+++ pl.springboot.crud-master/src/main/java/pl/springboot/crud/model/VerificationToken.java
@@ -15,9 +15,11 @@ import javax.persistence.OneToOne;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class VerificationToken {
     private static final int EXPIRATION = 60 * 24;
@@ -40,5 +42,14 @@ public class VerificationToken {
         cal.add(Calendar.MINUTE, expiryTimeInMinutes);
         return new Date(cal.getTime().getTime());
     }
+
+	public VerificationToken(String token, User user) {
+		super();
+		this.token = token;
+		this.user = user;
+		this.expiryDate = calculateExpiryDate(EXPIRATION);
+	}
+
+	
     
 }
